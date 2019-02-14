bf10e10b2fdb6e1bb302dbdd8e77e7d7a56c09c3
Tabeeb Yeamin <tabeebyeamin@gmail.com>
2019-02-14 01:42:04 -0500

    added some documentation

--- cheapestitems.py
+++ cheapestitems.py
@@ -1,6 +1,15 @@
 from cheapestprice import cheapest_price as cheapest
 
 def cheapest_product(my_url):
+    '''(url) -> tuple of string and float
+    This function takes a page url that has a list of products
+    and finds the cheapest product on that page.
+    REQ: The url must be a page url or else the program is unlikely
+    to work.
+    >>> cheapest_product('http://www.canadacomputers.com/index.php?cPath=21')
+    'Cheapest product is $6.49, the Elephant WEM-1015 (PLUS) Rigid Mic
+    Laser Mouse + Cable Clips, Black'
+    '''
     # start with the base page
     (cheapest_product, cheapest_price) = cheapest(my_url)
     # set the default value for the index
